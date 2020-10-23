d545550ad23ff071e604d24804b574ebf5f5db0f
Auden Childress <44931507+Geist-of-the-Automaton@users.noreply.github.com>
2020-10-23 19:58:33 -0400

    Ignore for now

---
+++ Roto/opencv_handler.h
@@ -0,0 +1,24 @@
+#ifndef OPENCV_HANDLER_H
+#define OPENCV_HANDLER_H
+
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <string>
+
+using std::string;
+using namespace cv;
+
+const int iLowH = 0, iHighH = 179, iLowS = 0, iHighS = 255, iLowV = 0, iHighV = 255;
+
+class OpenCV_Handler
+{
+public:
+    OpenCV_Handler();
+    void benDemo();
+
+};
+
+#endif // OPENCV_HANDLER_H
