d1f1947432cafa5aa4657af72955285f192b2430
MattPollard44 <mattpollard44@gmail.com>
2021-04-26 16:15:28 -0400

    Trying Mutex Lock to fix scroll crash

--- Roto/mainwindow.cpp
+++ Roto/mainwindow.cpp
@@ -260,6 +260,7 @@ void MainWindow::mouseDoubleClickEvent(QMouseEvent *event) {
 }
 
 void MainWindow::wheelEvent(QWheelEvent *event) {
+    scrollLock.lock();
     if (ioh->getWorkingLayer() == nullptr)
         return;
     int dy = event->angleDelta().y();
@@ -302,6 +303,7 @@ void MainWindow::wheelEvent(QWheelEvent *event) {
     }
     else if (mode == Raster_Mode)
         ioh->getWorkingLayer()->spinWheel(dy);
+    scrollLock.unlock();
     refresh();
 }
 
